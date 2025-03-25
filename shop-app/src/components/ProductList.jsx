8e4cf04cbb1897655a668a6542d3f664ad3f9a23
andriesvdw1 <andriesvdw1@gmail.com>
2025-03-25 22:02:26 +0200

    add more products to list. choose font colors for in stock/ unavailable statusses.

--- shop-app/src/components/ProductList.jsx
+++ shop-app/src/components/ProductList.jsx
@@ -14,7 +14,7 @@ function ProductList() {
     <section>
         { products.map((product) => (
             <div className="card" key={product.id}>
-                <p className='id'>{product.id}</p>
+                <p className='id' style={{backgroundColor:"blue", color:"white", width:"45px"}}>{product.id}</p>
                 <p className='name'>{product.name}</p>
                 <p className='info'>
                     <span>{product.price}</span>
